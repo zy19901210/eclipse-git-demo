f619eae8e138abcf699d2dc12e5baaa44f845d5c
Administrator <Administrator@USER-20170104OD>
2017-06-15 00:47:17 +0800

    Revert "d2v的分支提交"
    This reverts commit ac24710ac366e073662bd4a7c96c6b3c6c467e73.

--- git_test/src/git_test/Test_demo_git.java
+++ git_test/src/git_test/Test_demo_git.java
@@ -3,12 +3,6 @@ package git_test;
 public class Test_demo_git {
 public static void Test(){
 	System.out.println("aaaa");
-	System.out.println("maple1");
 	System.out.println("maple");
-	System.out.println("1111");
-	System.out.println("22");
-	System.out.println("333");
-	System.out.println("444");
-	System.out.println("555");
 }
 }
