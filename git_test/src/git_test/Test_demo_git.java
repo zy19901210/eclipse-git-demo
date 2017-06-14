c9ebb43ba248bf1e91481113c9ac82e649fa5203
Administrator <Administrator@USER-20170104OD>
2017-06-15 01:11:37 +0800

    d3v第一次提交

--- git_test/src/git_test/Test_demo_git.java
+++ git_test/src/git_test/Test_demo_git.java
@@ -4,6 +4,9 @@ public class Test_demo_git {
 public static void Test(){
 	System.out.println("aaaa");
 	System.out.println("maple1");
+	System.out.println("maple1");
+	System.out.println("maple1");
+	System.out.println("maple1");
 	System.out.println("maple");
 	System.out.println("1111");
 	System.out.println("22");
