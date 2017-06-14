2643e553d06622a846bd4ba84aeb622ebe3a17cc
Administrator <Administrator@USER-20170104OD>
2017-06-14 23:51:42 +0800

    第二次提交分支DEV

--- git_test/src/git_test/Test_demo_git.java
+++ git_test/src/git_test/Test_demo_git.java
@@ -3,5 +3,10 @@ package git_test;
 public class Test_demo_git {
 public static void Test(){
 	System.out.println("aaaa");
+	System.out.println("1111");
+	System.out.println("222");
+	System.out.println("3333");
+	System.out.println("4444");
+	System.out.println("555");
 }
 }
